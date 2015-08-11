990d3a5914495abda7ee7aa4b0f371fab68a3270
Ryan Casey <ryepdx@gmail.com>
2015-08-11 09:59:36 -0700

    Update gulpfile to deal with tooling changes.

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,12 @@ var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
 var concatCSS = require('gulp-concat-css');
 var mainBowerFiles = require('main-bower-files');
-var flatten = require('gulp-flatten');
+var replace = require('gulp-replace');
+var jshint = require('gulp-jshint');
+var rm = require('gulp-rm');
 
 var getFileGlobs = function () {
-    return ['src/lib/*.js'].concat(mainBowerFiles()).concat(['src/*.js']);
+    return ['src/lib/*.js'].concat(mainBowerFiles()).concat(['src/controllers/*.js', 'src/*.js', 'src/css/*.css']);
 };
 var allFiles = getFileGlobs();
 
@@ -16,11 +18,22 @@ gulp.task('watch', function () {
   gulp.watch(allFiles, ['build']);
 });
 
-gulp.task('build', function () {
+gulp.task('clean', function () {
+  return gulp.src(['build/**/*'], {read: false}).pipe(rm());
+});
+
+gulp.task('hint', function () {
+  return gulp.src(['src/*.js', 'src/**/*.js', '!src/lib', '!src/lib/**'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('build', ['clean', 'hint'], function () {
   allFiles = getFileGlobs();
   var jsFilter = filter('*.js', {restore: true});
   var cssFilter = filter('*.css', {restore: true});
-  var fontFilter = filter(['*.eot', '*.woff', '*.svg', '*.ttf']); 
+  var fontFilter = filter(['*.eot', '*.woff', '*.woff2', '*.svg', '*.ttf']); 
 
   return gulp.src(allFiles)
     .pipe(jsFilter)
@@ -30,10 +43,12 @@ gulp.task('build', function () {
     .pipe(jsFilter.restore)
     .pipe(cssFilter)
     .pipe(concatCSS('all.css'))
+    .pipe(replace(/font\/[^\/]+/ig, './build/fonts'))
     .pipe(minifyCSS())
     .pipe(gulp.dest('./build'))
     .pipe(cssFilter.restore)
     .pipe(fontFilter)
-    .pipe(flatten())
     .pipe(gulp.dest('./build/fonts'));
 });
+
+gulp.task('default', ['build', 'watch']);
