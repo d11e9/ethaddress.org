2bd0cd1aaafc86b1a4957c6d8e56a25e6c93387f
Ryan Casey <ryepdx@gmail.com>
2015-08-11 19:16:40 -0700

    Fix base58 encoding bug!

--- src/controllers/bulkWallets.js
+++ src/controllers/bulkWallets.js
@@ -17,7 +17,7 @@ EthPW.controllers.bulkWallets = function ($scope) {
     };
 
     $scope.base58 = function (hex) {
-      if (!hex || (Number.isNaN(parseInt(hex), 16))) return;
+      if (!hex || Number.isNaN(parseInt(hex, 16))) return;
       var intArray = [];
       for (var i=0; i < hex.length; i+=2) {
         intArray.push(parseInt(hex[i]+hex[i+1], 16));
